36a25c76bebfd6c82a73683feeb2b7f21c029c73
suyooktang <suyook.tang@gmail.com>
2016-10-31 19:38:46 +0900

    게시판 댓글 쓰기 기능 추가

--- www/skins/Femiwiki/resources/bbs.js
+++ www/skins/Femiwiki/resources/bbs.js
@@ -103,23 +103,91 @@ $(function () {
   function handleReadPage() {
     // Replace title
     var titleEl = document.querySelector('.firstHeading');
-    var title = titleEl.childNodes[0].nodeValue;
-    var match = title.match(/^(.+?):(.+?)( \([a-f0-9]+\))?$/);
+    var unparsedTitle = titleEl.childNodes[0].nodeValue;
+    var match = unparsedTitle.match(/^(.+?):(.+?)( \([a-f0-9]+\))?$/);
     var nsName = match[1];
-    var newTitle = match[2];
-    titleEl.childNodes[0].nodeValue = newTitle;
+    var title = match[2];
+    var hash = match[3];
+    titleEl.childNodes[0].nodeValue = title;
 
     // Render BBS menu
     $('#fw-footer-menu').prepend('<li><a href="#" class="list">글목록</a></li>');
     $('a.list').on('click', onClickList);
 
+
+    // Render comments
+    renderComments(nsName, title, hash, function($commentList) {
+      var $commentSection = $('<div id="fw-bbs-comment-section">');
+      var editable = mw.config.get('wgIsProbablyEditable');
+
+      $commentSection.append(
+        '<form id="fw-bbs-new-comment-form">' +
+        '<label for="fw-bbs-new-comment">새 댓글 쓰기:</label>' +
+        '<input type="text" id="fw-bbs-new-comment" ' + (editable ? '' : 'disabled') + ' placeholder="' + (editable ? '댓글' : '권한 없음') + '">' +
+        '</form>'
+      );
+      $commentSection.append($commentList);
+      $('#content').append($commentSection);
+
+      $('#fw-bbs-new-comment-form').on('submit', onComment);
+    });
+
     // Done
     $('body, #mw-wrapper').addClass('bbs-read');
+
+    function onComment(e) {
+      e.preventDefault();
+      var $comment = $('#fw-bbs-new-comment');
+      var comment = $comment.val();
+      $comment.val('');
+
+      if(!comment) return;
+
+      // Generate comment wikitext
+      var userName = mw.config.get('wgUserName');
+      var now = new Date();
+      var $newComment = $('<p><span class="ts"></span> <span class="user"></span> <span class="text"></span></p>');
+      $newComment.find('.ts').text(
+        now.getFullYear() + '-' +
+        zeropad(now.getMonth() + 1) + '-' +
+        zeropad(now.getDate()) + ' ' +
+        zeropad(now.getHours()) + ':' +
+        zeropad(now.getMinutes()) + ':' +
+        zeropad(now.getSeconds())
+      );
+      $newComment.find('.user').text('[[사용자:' + userName + '|' + userName + ']]');
+      $newComment.find('.text').text(comment);
+      var commentWikimarkup = $newComment.html();
+
+      // Prepend comment
+      $.getJSON('/w/api.php?action=query&meta=tokens&format=json', function(json) {
+        var token = json.query.tokens.csrftoken;
+        var discussionTitle = nsName + '토론:' + title + ' ' + hash;
+        $.post('/w/api.php?action=edit', {summary: '댓글: ' + comment, title: discussionTitle, prependtext: '* ' + commentWikimarkup + '\n', token: token}, function() {
+          // reload comment
+          renderComments(nsName, title, hash, function($commentList) {
+            $('#fw-bbs-comment-list').html($commentList.html());
+          });
+        });
+      });
+    }
 
     function onClickList(e) {
       e.preventDefault();
 
       location.href = '/w/' + encodeTitle('페미위키:' + nsName) + '?classes=bbs-list';
+    }
+
+    function renderComments(nsName, title, hash, callback) {
+      var $commentList = $('<ol id="fw-bbs-comment-list" />');
+      $commentList.load(getDiscussionUrl(nsName, title, hash) + ' #mw-content-text > ul > li', function(res, status, xhr) {
+        callback($commentList);
+      });
+    }
+
+    function getDiscussionUrl(nsName, title, hash) {
+      var unparsedTitle = nsName + '토론:' + title + ' ' + hash + '';
+      return '/w/' + encodeTitle(unparsedTitle);
     }
   }
 
