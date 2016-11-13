06bac07855e35f1ca387156ccb3940fe4b46b7a4
suyooktang <suyook.tang@gmail.com>
2016-11-13 13:09:01 +0900

    Add CSS for calendar

--- www/LocalSettings.php
+++ www/LocalSettings.php
@@ -38,7 +38,7 @@ $wgEnableCanonicalServerLink = true;
 ## The URL path to static resources (images, scripts, etc.)
 $wgResourceBasePath = $wgScriptPath;
 
-$wgStyleVersion = '20161101_2';
+$wgStyleVersion = '20161113_1';
 $wgResourceLoaderMaxage = array(
     'versioned' => array(
         // Squid/Varnish but also any other public proxy cache between the client and MediaWiki
