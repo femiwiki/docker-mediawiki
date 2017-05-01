7d04ccf395385c86165c836bc10a50413f550a0f
Lens <lorentz0021@gmail.com>
2017-05-01 13:30:31 +0900

    누락된 언어 복귀

--- www/extensions/FacetedCategory/FacetedCategories.alias.php
+++ www/extensions/FacetedCategory/FacetedCategories.alias.php
@@ -2,6 +2,11 @@
 
 $specialPageAliases = array();
 
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'FacetedCategories' => array( 'FacetedCategories' ),
+);
+
 /** Korean (한국어) */
 $specialPageAliases['ko'] = array(
 	'FacetedCategories' => array( '다면분류' ),
