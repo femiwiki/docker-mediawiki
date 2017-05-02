edac23e997a2b7b68e9e7a77c2f2d96eab2d66f4
Lens <lorentz0021@gmail.com>
2017-05-02 15:10:10 +0900

    Update SpecialFacetedCategories.php

--- www/extensions/FacetedCategory/specials/SpecialFacetedCategories.php
+++ www/extensions/FacetedCategory/specials/SpecialFacetedCategories.php
@@ -1,6 +1,6 @@
 <?php
 
-class SpecialFacetedCategories extends SpecialPage {
+class SpecialFacetedCategories extends IncludableSpecialPage {
 
 	protected $linkRenderer = null;
 
@@ -14,13 +14,14 @@ class SpecialFacetedCategories extends SpecialPage {
         $this->linkRenderer = $linkRenderer;
     }
     
-private function initServices() {
-    if ( !$this->linkRenderer ) {
-        $lang = $this->getContext()->getLanguage();
-        $titleFormatter = new MediaWikiTitleCodec( $lang, GenderCache::singleton() );
-        $this->linkRenderer = new MediaWikiPageLinkRenderer( $titleFormatter );
-    }
-}
+	private function initServices() {
+	    if ( !$this->linkRenderer ) {
+	        $lang = $this->getContext()->getLanguage();
+	        $titleFormatter = new MediaWikiTitleCodec( $lang, GenderCache::singleton() );
+	        $this->linkRenderer = new MediaWikiPageLinkRenderer( $titleFormatter );
+	    }
+	}
+
 
 	public function execute( $par ) {
 		$this->initServices();
@@ -29,31 +30,31 @@ private function initServices() {
 		$this->outputHeader();
 		$this->getOutput()->allowClickjacking();
 
-
 		$slash = strpos($par,'/');
 		$left = $slash===false?$par:substr($par,0,$slash);
-		$right = substr($par,$slash+1,strlen($par)-1);
+		$right = $slash===false?'':substr($par,$slash+1,strlen($par)-1);
 		
 		$facetName = $this->getRequest()->getText( 'facetName', $left );
 		$facetMember = $this->getRequest()->getText( 'facetMember', $right );
-		$matchExactly = $this->getRequest()->getBool( 'matchExactly', false );
+		$matchExactly = $this->getRequest()->getBool( 'matchExactly', true );
 
 		$cap = new FacetedCategoryPager(
 			$this->getContext(),
 			$facetName,
 			$facetMember,
 			$matchExactly,
-			$this->linkRenderer
+			$this->linkRenderer,
+			$this->including()
 		);
 		$cap->doQuery();
 
 		$this->getOutput()->addHTML(
 			Html::openElement( 'div', [ 'class' => 'mw-spcontent' ] ) .
-				$this->msg( 'categoriespagetext', $cap->getNumRows() )->parseAsBlock() .
+				($this->including()?'':$this->msg( 'categoriespagetext', $cap->getNumRows() )->parseAsBlock()) .
 				$cap->getStartForm( $facetName, $facetMember, $matchExactly ) .
-				$cap->getNavigationBar() .
+				($this->including()?'':$cap->getNavigationBar()) .
 				'<ul>' . $cap->getBody() . '</ul>' .
-				$cap->getNavigationBar() .
+				($this->including()?'':$cap->getNavigationBar()) .
 				Html::closeElement( 'div' )
 		);
 	}
