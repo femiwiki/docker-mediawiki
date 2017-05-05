ac8c909b09a11def55a60a8d83108c727dd8f84f
lens0021 <lorentz0021@gmail.com>
2017-05-05 12:34:06 +0900

    디렉토리 만들기
    이 부분이 없으면 cp가 실패하는 것 같습니다. 확인 부탁드립니다.

--- parsoid/provision.sh
+++ parsoid/provision.sh
@@ -2,6 +2,8 @@
 sudo apt-key advanced --keyserver pgp.mit.edu --recv-keys 90E9F83F22250DD7
 sudo apt-add-repository "deb https://releases.wikimedia.org/debian jessie-mediawiki main"
 sudo apt-get update && sudo apt-get install -y --force-yes parsoid
+sudo mkdir /etc/mediawiki
+sudo mkdir /etc/mediawiki/parsoid/
 sudo cp /vagrant/parsoid/settings.js /etc/mediawiki/parsoid/settings.js
 sudo sed -i s/PROTOCOL/$1/ /etc/mediawiki/parsoid/settings.js
 sudo sed -i s/HOST/$2/ /etc/mediawiki/parsoid/settings.js
