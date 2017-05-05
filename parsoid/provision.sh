1f31f56ebafc0e350817f5857adc527377d5a209
lens0021 <lorentz0021@gmail.com>
2017-05-05 16:33:59 +0900

    provision에 nodejs 설치 및 config.yaml 작성 명시

--- parsoid/provision.sh
+++ parsoid/provision.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs npm
 sudo apt-key advanced --keyserver pgp.mit.edu --recv-keys 90E9F83F22250DD7
 sudo apt-add-repository "deb https://releases.wikimedia.org/debian jessie-mediawiki main"
 sudo apt-get update && sudo apt-get install -y --force-yes parsoid
-sudo mkdir /etc/mediawiki
-sudo mkdir /etc/mediawiki/parsoid/
 sudo cp /vagrant/parsoid/settings.js /etc/mediawiki/parsoid/settings.js
 sudo sed -i s/PROTOCOL/$1/ /etc/mediawiki/parsoid/settings.js
 sudo sed -i s/HOST/$2/ /etc/mediawiki/parsoid/settings.js
+sudo cp /vagrant/parsoid/config.yaml /etc/mediawiki/parsoid/config.yaml
+sudo sed -i s/PROTOCOL/$1/ /etc/mediawiki/parsoid/config.yaml
+sudo sed -i s/HOST/$2/ /etc/mediawiki/parsoid/config.yaml
 
 sudo service parsoid restart
 
