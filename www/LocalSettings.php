80abb1ab7eedce6e53666448e653dd269604ddb1
suyooktang <suyook.tang@gmail.com>
2017-06-04 17:34:31 +0900

    Break long words in RC

--- www/LocalSettings.php
+++ www/LocalSettings.php
@@ -38,7 +38,7 @@ $wgEnableCanonicalServerLink = true;
 ## The URL path to static resources (images, scripts, etc.)
 $wgResourceBasePath = $wgScriptPath;
 
-$wgStyleVersion = '20170603_1';
+$wgStyleVersion = '20170604_1';
 $wgResourceLoaderMaxage = array(
     'versioned' => array(
         // Squid/Varnish but also any other public proxy cache between the client and MediaWiki
