4f916df351c54b8ce92dbc46364cc061e0018e97
lens0021 <lorentz0021@gmail.com>
2018-09-23 15:55:03 +0900

    Composer 설치

--- www/provision.sh
+++ www/provision.sh
@@ -25,6 +25,12 @@ if [ ! -f /opt/femiwiki-provisioned ]; then
         librsvg2-bin
     sudo apt-get --purge autoremove -y
 
+    # Install Composer
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+
     # Download Mediawiki source
     wget -nv https://releases.wikimedia.org/mediawiki/1.31/mediawiki-1.31.1.tar.gz
     sudo mkdir /var/www/femiwiki.com
