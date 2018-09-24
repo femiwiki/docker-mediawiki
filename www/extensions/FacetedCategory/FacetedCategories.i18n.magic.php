22fb62b5a623707fe4654accbf2be91add12fa06
lens0021 <lorentz0021@gmail.com>
2018-09-24 18:14:44 +0900

    사용량이 극도로 적은 특수:패싯문서 제거

--- www/extensions/FacetedCategory/FacetedCategories.i18n.magic.php
+++ www/extensions/FacetedCategory/FacetedCategories.i18n.magic.php
@@ -3,14 +3,12 @@ $magicWords = array();
 
 /** English (English) */
 $magicWords['en'] = array(
-	'facetedcategories' => array( 0, 'facetedcategories' ),
-	'facets' => array( 0, 'facets' ),
+	'facetedcategories' => array( 0, 'facetedcategories' )
 );
 
 /** Korean (한국어) */
 $magicWords['ko'] = array(
-	'facetedcategories' => array( 0, '다면분류' ),
-	'facets' => array( 0, '패싯목록' ),
+	'facetedcategories' => array( 0, '다면분류' )
 );
 
 ?>
