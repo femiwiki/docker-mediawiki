4da0c09f787de7133b0f093376c2065340ac504d
lens0021 <lorentz0021@gmail.com>
2018-09-26 10:50:34 +0900

    minor fix

--- www/extensions/UnifiedExtensionForFemiwiki/Hooks.php
+++ www/extensions/UnifiedExtensionForFemiwiki/Hooks.php
@@ -4,6 +4,7 @@ class FemiwikiHooks {
 
 	/**
 	 * 푸터에 몇 링크들을 추가합니다.
+	 * @return bool Sends a line to the debug log if false.
 	 */
 	public static function onSkinTemplateOutputPageBeforeExec( &$skin, &$template ) {
 		// 이용 약관을 추가합니다. 이용 약관은 앞쪽에 추가합니다.
@@ -13,7 +14,7 @@ class FemiwikiHooks {
 		// 권리 침해 신고를 추가합니다.
 		$template->set( 'femiwiki-report-infringement-label', $template->getSkin()->footerLink( 'femiwiki-report-infringement-label', 'femiwiki-report-infringement-page' ) );
 		$template->data['footerlinks']['places'][] = 'femiwiki-report-infringement-label';
+
+		return true;
 	}
 }
-
-?>
