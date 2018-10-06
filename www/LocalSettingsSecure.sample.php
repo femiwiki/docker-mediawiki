bbd8b18c81ec095110b86b93fd3c7492c03008da
Hyeon Kim <simnalamburt@gmail.com>
2018-10-06 23:23:41 +0900

    LocalSettingsSecure.sample.php 업데이트

--- www/LocalSettingsSecure.sample.php
+++ www/LocalSettingsSecure.sample.php
@@ -1,8 +1,8 @@
 <?php
 # Database settings
-$wgDBserver = 'localhost';
+$wgDBserver = 'DB-HOST-NAME';
 $wgDBuser = 'root';
-$wgDBpassword = 'root';
+$wgDBpassword = 'DB-PASSWORD';
 
 # Mail
 $wgSMTP = [
@@ -24,3 +24,6 @@ $wgUpgradeKey = "UPGRADE-KEY";
 # 점검이 끝나면 아래 라인 주석처리한 뒤, 아래 문서 내용을 비우면 됨
 # https://femiwiki.com/w/%EB%AF%B8%EB%94%94%EC%96%B4%EC%9C%84%ED%82%A4:Sitenotice
 # $wgReadOnly = '데이터베이스 업그레이드 작업이 진행 중입니다. 작업이 진행되는 동안 사이트 이용이 제한됩니다.';
+
+# 업로드를 막고싶을때엔 아래 라인 주석 해제하면 됨
+# $wgEnableUploads = false;
