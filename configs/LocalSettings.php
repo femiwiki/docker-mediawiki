de1ad2f92131d71de25efe032cbeced5d65088e0
Naggyeop <lorentz0021@gmail.com>
2019-02-18 00:07:43 +0900

    Remove Legacy BBSes
    Issue: https://github.com/femiwiki/mediawiki/issues/214

--- configs/LocalSettings.php
+++ configs/LocalSettings.php
@@ -158,16 +158,6 @@ define( 'NS_BBS_TALK', 3907 );
 $wgExtraNamespaces[NS_BBS] = '게시판';
 $wgExtraNamespaces[NS_BBS_TALK] = '게시판토론';
 
-# BBS (Legacy)
-define( 'NS_BBSFREE', 3902 );
-define( 'NS_BBSFREE_TALK', 3903 );
-$wgExtraNamespaces[NS_BBSFREE] = '자유게시판';
-$wgExtraNamespaces[NS_BBSFREE_TALK] = '자유게시판토론';
-define( 'NS_BBSINTRO', 3904 );
-define( 'NS_BBSINTRO_TALK', 3905 );
-$wgExtraNamespaces[NS_BBSINTRO] = '가입인사게시판';
-$wgExtraNamespaces[NS_BBSINTRO_TALK] = '가입인사게시판토론';
-
 # Permission
 $wgGroupPermissions['*']['createaccount'] = true;
 $wgGroupPermissions['sysop']['usermerge'] = true;
