9d694ced565b3d80c1908a935da9679407225645
lens0021 <lorentz0021@gmail.com>
2019-07-10 20:49:02 +0900

    Add single space between "#" and comment

--- configs/LocalSettings.php
+++ configs/LocalSettings.php
@@ -277,8 +277,8 @@ $wgVirtualRestConfig['modules']['parsoid'] = [
 # Restbase server Setting
 # 잠시 끔. https://github.com/femiwiki/femiwiki/issues/82 참고
 # $wgVirtualRestConfig['modules']['restbase'] = [
-# 	 'url' => 'http://restbase:7231',
-#	 'domain' => 'femiwiki.com'
+# 	'url' => 'http://restbase:7231',
+# 	'domain' => 'femiwiki.com'
 # ];
 # $wgVisualEditorRestbaseURL = 'https://femiwiki.com/femiwiki.com/v1/page/html/';
 # $wgVisualEditorFullRestbaseURL = 'https://femiwiki.com/femiwiki.com/';
