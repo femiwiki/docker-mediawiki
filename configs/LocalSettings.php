56c9fa0802a93c4d438acffc65a8c0dd3f16d16c
Lens0021 <lorentz0021@gmail.com>
2019-11-06 22:41:34 +0900

    Update LocalSettings.php

--- configs/LocalSettings.php
+++ configs/LocalSettings.php
@@ -170,7 +170,7 @@ $wgFemiwikiHeadItems = [
 	'fav16' => "<meta name='msapplication-TileImage' content='/fw-resources/favicons/ms-icon-144x144.png'>",
 	'fav17' => "<meta name='theme-color' content='#ffffff'>"
 ];
-$wgFemiwikiTwitterAccount = 'femiwikidotcometest';
+$wgFemiwikiTwitterAccount = 'femiwikidotcome';
 
 #
 # Namespace settings
