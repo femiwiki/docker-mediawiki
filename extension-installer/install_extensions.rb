4948204af2aab39b2d25d4face10b0a43a7dba24
Lens0021 <lorentz0021@gmail.com>
2019-11-16 11:56:02 +0900

    DiscordNotifications 포크 사용

--- extension-installer/install_extensions.rb
+++ extension-installer/install_extensions.rb
@@ -82,7 +82,7 @@ skins_official = [
 # 3rd party extensions and their URLs
 extensions_3rdparty = {
   'AWS' => 'https://github.com/edwardspec/mediawiki-aws-s3/archive/v0.10.0.tar.gz',
-  'DiscordNotifications' => 'https://github.com/kulttuuri/DiscordNotifications/archive/1.11.tar.gz',
+  'DiscordNotifications' => 'https://github.com/femiwiki/DiscordNotifications/archive/master.tar.gz',
   'EmbedVideo' => 'https://gitlab.com/hydrawiki/extensions/EmbedVideo/-/archive/v2.8.0/EmbedVideo-v2.8.0.tar.gz',
   'SimpleMathJax' => 'https://github.com/jmnote/SimpleMathJax/archive/v0.7.4.tar.gz',
 }
