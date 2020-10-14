836dc03762a0ecd5a9f8f9a287d6c27ea22a41b5
lens0021 <lorentz0021@gmail.com>
2020-10-14 20:53:08 +0900

    Tweak DevelopmentSettings

--- configs/LocalSettings.php
+++ configs/LocalSettings.php
@@ -965,6 +965,9 @@ if ( defined( 'DEBUG_MODE' ) ) {
 
 	// 디버그 툴 활성화
 	require_once "includes/DevelopmentSettings.php";
+	// Overwrite DevelopmentSettings
+	$wgCacheDirectory = '/tmp/cache';
+
 	$wgDebugToolbar = true;
 	$wgShowDBErrorBacktrace = true;
 
