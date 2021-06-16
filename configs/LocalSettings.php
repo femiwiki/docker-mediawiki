d55a6d8be2808d12001bce5d827ff7e533780149
lens0021 <lorentz0021@gmail.com>
2021-06-16 17:25:45 +0900

    Add meta tags
    https://github.com/femiwiki/docker-mediawiki/issues/549

--- configs/LocalSettings.php
+++ configs/LocalSettings.php
@@ -163,13 +163,20 @@ $wgPageLanguageUseDB = true;
 // Changing this will log out all existing sessions.
 $wgAuthenticationTokenVersion = '1';
 
-## For attaching licensing metadata to pages, and displaying an
-## appropriate copyright notice / icon. GNU Free Documentation
-## License and Creative Commons licenses are supported so far.
+// For attaching licensing metadata to pages, and displaying an
+// appropriate copyright notice / icon. GNU Free Documentation
+// License and Creative Commons licenses are supported so far.
 $wgRightsPage = '페미위키:저작권';
 $wgRightsUrl = 'https://creativecommons.org/licenses/by-sa/4.0/deed.ko';
 $wgRightsText = '크리에이티브 커먼즈 저작자표시-동일조건변경허락 4.0 국제 라이선스';
 $wgRightsIcon = "$wgResourceBasePath/resources/assets/licenses/cc-by-sa.png";
+
+// Open graph tags which should be added by all skins
+$wgSkinMetaTags = [
+	'og:title',
+	'og:type',
+	'twitter:card',
+];
 
 // Path to the GNU diff3 utility. Used for conflict resolution.
 $wgDiff3 = '/usr/bin/diff3';
